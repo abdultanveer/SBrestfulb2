62f220dbe5432ff0e776efc194320105a417b84f
abdul <abdul.tanveer@gmail.com>
2021-11-02 10:31:03 +0530

    student pojo student service -- returning hardcoded students

--- SBrestb2/src/main/java/dxc/b2/rest/StudentController.java
+++ SBrestb2/src/main/java/dxc/b2/rest/StudentController.java
@@ -1,10 +1,31 @@
 package dxc.b2.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * controller controls the incoming requests ie it maps the url to a java method
+ * @author Admin
+ *
+ */
 @RestController
 public class StudentController {
+	
+	@Autowired
+	StudentService studentService;
+	
+	@RequestMapping("/students")
+	public List<Student> getStudents(){
+
+		return studentService.getStudents();
+
+	}
+
 	
 	@GetMapping("/")
 	public String home() {
